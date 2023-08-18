a051d96f40da717dfbef7e2810737d142b4bf635
SalehTrissi <Rayatrissi1@gmail.com>
2023-08-18 19:55:24 +0200

    Update the pog in JS and clean up the code by not repeat

--- LITRevu/static/js/script.js
+++ LITRevu/static/js/script.js
@@ -2,24 +2,37 @@ document.addEventListener("DOMContentLoaded", function() {
     const critiquesLink = document.getElementById("critiques-link");
     const ticketsLink = document.getElementById("tickets-link");
     const selectedOption = document.getElementById("selected-option");
-    const ticketContent = document.getElementById("is-ticket");
-    const reviewContent = document.getElementById("is-review");
-    const allLink = document.getElementById("all-link")
+    const ticketContent = document.getElementsByClassName("is-ticket");
+    const reviewContent = document.getElementsByClassName("is-review");
+    const allLink = document.getElementById("all-link");
     
     critiquesLink.addEventListener("click", function() {
         selectedOption.textContent = "Critiques";
-        ticketContent.style.display = "none";
-        reviewContent.style.display = "block";
+        for (const ticket of ticketContent) {
+            ticket.style.display = "none";
+        }
+        for (const review of reviewContent) {
+            review.style.display = "block";
+        }
     });
     
     ticketsLink.addEventListener("click", function() {
         selectedOption.textContent = "Tickets";
-        reviewContent.style.display = "none";
-        ticketContent.style.display = "block";
+        for (const review of reviewContent) {
+            review.style.display = "none";
+        }
+        for (const ticket of ticketContent) {
+            ticket.style.display = "block";
+        }
     });
+    
     allLink.addEventListener("click", function () {
         selectedOption.textContent = "Tous";
-        ticketContent.style.display = "block";
-        reviewContent.style.display = "block";
-    })
+        for (const item of ticketContent) {
+            item.style.display = "block";
+        }
+        for (const item of reviewContent) {
+            item.style.display = "block";
+        }
+    });
 });
